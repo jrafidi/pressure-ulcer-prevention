bf62e5b8171b6b976e7311edcf489516867630e3
Joey Rafidi <jfidi731@gmail.com>
2013-11-03 21:59:51 -0500

    Change to arduino code push rate

--- arduino/AccelToSerial/AccelToSerial.ino
+++ arduino/AccelToSerial/AccelToSerial.ino
@@ -10,11 +10,10 @@ void loop()
   x = analogRead(0);       // read analog input pin 0
   y = analogRead(1);       // read analog input pin 1
   z = analogRead(2);       // read analog input pin 1
-  Serial.print("accelerations are x, y, z: ");
   Serial.print(x, DEC);    // print the acceleration in the X axis
   Serial.print(" ");       // prints a space between the numbers
   Serial.print(y, DEC);    // print the acceleration in the Y axis
   Serial.print(" ");       // prints a space between the numbers
   Serial.println(z, DEC);  // print the acceleration in the Z axis
-  delay(100);              // wait 100ms for next reading
+  delay(1000);              // wait 100ms for next reading
 }
