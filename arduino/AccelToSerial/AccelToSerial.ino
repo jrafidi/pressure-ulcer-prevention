563cde380a356192031477ffd4e002d17bd20d76
Joey Rafidi <jfidi731@gmail.com>
2013-11-30 14:18:41 -0500

    update arduino code

--- arduino/AccelToSerial/AccelToSerial.ino
+++ arduino/AccelToSerial/AccelToSerial.ino
@@ -1,4 +1,4 @@
-int x1, y1, z1, x2, y2, z2;
+int x1, y1, z1, x2, y2, z2, x3, y3, z3;
 
 void setup()
 {
@@ -13,6 +13,9 @@ void loop()
   x2 = analogRead(3);       // read analog input pin 3
   y2 = analogRead(4);       // read analog input pin 4
   z2 = analogRead(5);       // read analog input pin 5
+  x3 = analogRead(6);       // read analog input pin 6
+  y3 = analogRead(7);       // read analog input pin 7
+  z3 = analogRead(8);       // read analog input pin 8
   Serial.print("[");
   Serial.print(x1, DEC);    // print the acceleration in the X axis
   Serial.print(",");       // prints a space between the numbers
@@ -25,6 +28,12 @@ void loop()
   Serial.print(y2, DEC);    // print the acceleration in the Y axis
   Serial.print(",");       // prints a space between the numbers
   Serial.print(z2, DEC);  // print the acceleration in the Z axis
+  Serial.print(",");       // prints a space between the numbers
+  Serial.print(x3, DEC);    // print the acceleration in the X axis
+  Serial.print(",");       // prints a space between the numbers
+  Serial.print(y3, DEC);    // print the acceleration in the Y axis
+  Serial.print(",");       // prints a space between the numbers
+  Serial.print(z3, DEC);  // print the acceleration in the Z axis
   Serial.println("]");
   delay(1000);              // wait 1s for next reading
 }
