785ccbe3d9c4bfefd5e0e5e72b053fe0a7e09044
Joey Rafidi <jrafidi@palantir.com>
2014-05-04 00:04:33 -0400

    ble searching

---
+++ Research Device/raspberry-pi/test_scripts/bluetooth_search_test.py
@@ -0,0 +1,8 @@
+from subprocess import Popen, PIPE
+
+proc = Popen(["hcitool", "lescan"], stdout=PIPE, bufsize=1) # start process
+for line in iter(proc.stdout.readline, b''): # read output line-by-line
+    print line,
+# reached EOF, nothing more to read
+proc.communicate() # close `proc.stdout`, wait for child process to terminate
+print "Exit status", proc.returncode
